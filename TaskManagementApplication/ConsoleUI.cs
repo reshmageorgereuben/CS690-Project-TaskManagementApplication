4516b03eb8878fb48d698d807913896361698958
reshmageorgereuben <153766324+reshmageorgereuben@users.noreply.github.com>
2026-04-30 17:25:19 +0000

    Adding small changes

--- TaskManagementApplication/ConsoleUI.cs
+++ TaskManagementApplication/ConsoleUI.cs
@@ -29,12 +29,47 @@ public class ConsoleUI{
                 bool result;
                 AnsiConsole.MarkupLine("Please enter task details");
                 do {                    
-                    var taskName = AnsiConsole.Prompt(new TextPrompt<string>("Task Name:"));
-                    var description = AnsiConsole.Prompt(new TextPrompt<string>("Task Description:"));
-                    DateTime deadline = AnsiConsole.Prompt(new TextPrompt<DateTime>("DeadLine:"));                    
+                    var taskName = AnsiConsole.Prompt(new TextPrompt<string>("Task Name:").Validate(input =>{
+                            return input.Contains("|")
+                                ? ValidationResult.Error("[red]Please do not use | symbol.[/]")
+                                : ValidationResult.Success();
+                        }));
+                    var description = AnsiConsole.Prompt(new TextPrompt<string>("Task Description:").Validate(input => {
+                                return input.Contains("|")
+                                    ? ValidationResult.Error("[red]Please do not use | symbol.[/]")
+                                    : ValidationResult.Success();
+                            }));
+                    // DateTime deadline = AnsiConsole.Prompt(new TextPrompt<DateTime>("DeadLine:"));   
+                      DateTime deadline = AnsiConsole.Prompt(new TextPrompt<DateTime>("Deadline:").Validate(date => {
+                                           return date.Date >= DateTime.Today
+                                                ? ValidationResult.Success()
+                                                : ValidationResult.Error("[red]Date must be today or later[/]");
+                                        
+                            }));
+                     string priority = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Priority")
+                        .AddChoices("None","High", "Medium", "Low"));
+                        AnsiConsole.MarkupLine($"Selected Priority: [white]{priority}[/]");
+                    var category =  AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Category")
+                        .AddChoices("None","Work", "Personal", "Errand"));
+                        AnsiConsole.MarkupLine($"Selected Category: [white]{category}[/]");
+                    var status =  AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("Status")
+                        .AddChoices("None","To Do", "In Progress", "Completed"));
+                        AnsiConsole.MarkupLine($"Selected Status: [white]{status}[/]");
+                    DateTime starttime = AnsiConsole.Prompt(new TextPrompt<DateTime>("Start Time:").AllowEmpty());   
+                    DateTime endtime = AnsiConsole.Prompt(new TextPrompt<DateTime>("End Time:").AllowEmpty());   
 
-                    Task newTask = new Task(0,taskName,description,deadline);
-                    dataManager.addtasksToList(newTask);
+                   
+                       Task newTask = new Task(0,taskName,description,deadline,priority,category,starttime,endtime,status);
+                        dataManager.addtasksToList(newTask); 
+                        AnsiConsole.MarkupLine("[green]Task Created Successfully[/]");
+                    
+                    
 
                     result = AnsiConsole.Confirm("Do you want to continue?");
                 } while(result);
@@ -55,18 +90,20 @@ public class ConsoleUI{
                     table.AddColumn("Category");
                     table.AddColumn("Start Time");
                     table.AddColumn("End Time");
+                    table.AddColumn("Status");
                     foreach (var task in taskListItems)
                     {
                         table.AddRow(
 
                             task.TaskId.ToString(),
-                            task.TaskName,
-                            task.Description,
+                            task.TaskName ?? "",
+                            task.Description ?? "",
                             task.Deadline.ToShortDateString(),
-                            task.Priority,
-                            task.Category,
-                             "",
-                             ""                          
+                            task.Priority ?? "",
+                            task.Category ?? "",
+                            task.StartTime.ToString() ?? "",
+                            task.EndTime.ToString() ?? "",
+                            task.Status ?? ""
 
 
                         );
@@ -84,25 +121,70 @@ public class ConsoleUI{
                        string operation = AnsiConsole.Prompt(
                         new SelectionPrompt<string>()
                         .Title("Please enter your operation")
-                        .AddChoices( "Update Task", "Delete Task", "Set Priority", "Set Category", "Schedule Time block", "End"));
+                        .AddChoices( "Update Task", "Delete Task", "Set Priority", "Set Category", "Schedule Time block", "Track Time", "End"));
 
 
                         if(operation == "Update Task") {
-                                string newName = AnsiConsole.Prompt( new TextPrompt<string>("New Task Name:").DefaultValue(editTask.TaskName));
-                                string newDescription = AnsiConsole.Prompt( new TextPrompt<string>("New Description:").DefaultValue(editTask.Description));
-                                DateTime newDeadLine = AnsiConsole.Prompt( new TextPrompt<DateTime>("New DeadLine").DefaultValue(editTask.Deadline));
+                                string newName = AnsiConsole.Prompt( new TextPrompt<string>("New Task Name:").DefaultValue(editTask.TaskName).Validate(input => {
+                                                return input.Contains("|")
+                                                    ? ValidationResult.Error("[red]Please do not use | symbol.[/]")
+                                                    : ValidationResult.Success();
+                                            }));
+                                string newDescription = AnsiConsole.Prompt( new TextPrompt<string>("New Description:").DefaultValue(editTask.Description).Validate(input => {
+                                                return input.Contains("|")
+                                                    ? ValidationResult.Error("[red]Please do not use | symbol.[/]")
+                                                    : ValidationResult.Success();
+                                            }));
+                                 DateTime newDeadLine = AnsiConsole.Prompt( new TextPrompt<DateTime>("New DeadLine").DefaultValue(editTask.Deadline).Validate(date =>
+                                            date.Date >= DateTime.Today
+                                                ? ValidationResult.Success()
+                                                : ValidationResult.Error("[red]Date must be today or later[/]")
+                                        ));
+                                // DateTime newDeadLine = AnsiConsole.Prompt(
+                                //     new TextPrompt<DateTime>($"New Deadline ( [green]{editTask.Deadline}[/])")
+                                //         .Validate(date =>
+                                //             date.Date >= DateTime.Today
+                                //                 ? ValidationResult.Success()
+                                //                 : ValidationResult.Error("[red]Date must be today or later[/]")
+                                //         )
+                                // );
+                                 string newPriority = AnsiConsole.Prompt(
+                                    new SelectionPrompt<string>()
+                                    .Title($"Please select your  new priority ([green]{editTask.Priority}[/])")
+                                    .AddChoices("None","High", "Medium", "Low").DefaultValue(editTask.Priority));
+                                     AnsiConsole.MarkupLine($"Selected Priority: [white]{newPriority}[/]");
+                                var newCategory =  AnsiConsole.Prompt(
+                                    new SelectionPrompt<string>()
+                                    .Title($"Please select your new category ([green]{editTask.Category}[/])")
+                                    .AddChoices("None","Work", "Personal", "Errand").DefaultValue(editTask.Category));
+                                     AnsiConsole.MarkupLine($"Selected Priority: [white]{newCategory}[/]");
+                                   var newStatus =   AnsiConsole.Prompt( new SelectionPrompt<string>()
+                                    .Title($"Please select your new status ([green]{editTask.Status}[/])")
+                                    .AddChoices("None","To Do", "In Progress", "Completed").DefaultValue(editTask.Status));
+                                    AnsiConsole.MarkupLine($"Selected Status: [white]{newStatus}[/]");
+                               
+                                DateTime? newStartTime = AskOptionalDate("New Start Time:", editTask.StartTime);
+                                DateTime? newEndTime = AskOptionalDate("New End Time:", editTask.EndTime);
+                              
 
-                             
+                               
+                               
                                 foreach(var eachItem in dataManager.TaskList ){
                                     if(eachItem.TaskId == editTask.TaskId){
                                         eachItem.TaskName = newName;
                                         eachItem.Description = newDescription;
                                         eachItem.Deadline = newDeadLine;
+                                        eachItem.Priority = newPriority;
+                                        eachItem.Category = newCategory;
+                                        eachItem.StartTime = newStartTime;
+                                        eachItem.EndTime = newEndTime;
+                                        eachItem.Status = newStatus;
                                     }
                                 }
                                 
                                 
                               dataManager.SaveAllData();
+                              AnsiConsole.MarkupLine("[green]Task Updated Successfully[/]");
                                 
 
                         } else if(operation == "Delete Task") {                            
@@ -111,45 +193,78 @@ public class ConsoleUI{
                              if(confirmDelete){
                                
                                  dataManager.TaskList.RemoveAll(a => a.TaskId == id);
+                                dataManager.SaveAllData();
+                                AnsiConsole.MarkupLine("[green]Task Deleted Successfully[/]");
                                  
                              }
-                            dataManager.SaveAllData();
+                           
                             
-                        } else if(operation == "Set Priority") {                            
-                             
-                             string priority = AnsiConsole.Prompt(
-                        new SelectionPrompt<string>()
-                        .Title("Please select your priority")
-                        .AddChoices( "High", "Medium", "Low"));
-                             foreach(var eachItem in dataManager.TaskList ){
-                                    if(eachItem.TaskId == editTask.TaskId){
+                        } else if(operation == "Set Priority") { 
+
+                                foreach (var eachItem in dataManager.TaskList)
+                                {
+                                    if (eachItem.TaskId == editTask.TaskId)
+                                    {   
+                                        AnsiConsole.WriteLine($"Current Priority:{eachItem.Priority}");
+                                        
+                                        string priority = AnsiConsole.Prompt(
+                                            new SelectionPrompt<string>()
+                                                .Title("Please select your priority")
+                                                .AddChoices("High", "Medium", "Low")
+                                        );
+                                        AnsiConsole.WriteLine($"Selected Priority:{priority}");
                                         eachItem.Priority = priority;
-                                        
                                     }
                                 }
-                            dataManager.SaveAllData();
+
+
+                      
+                          dataManager.SaveAllData();
+                          AnsiConsole.MarkupLine("[green]Priority set successfully[/]");
                             
-                        } else if(operation == "Set Category") {                            
-                             
-                             string category = AnsiConsole.Prompt(
-                        new SelectionPrompt<string>()
-                        .Title("Please select your priority")
-                        .AddChoices( "Work", "Personal", "Errand"));
-                             foreach(var eachItem in dataManager.TaskList ){
-                                    if(eachItem.TaskId == editTask.TaskId){
-                                        eachItem.Category = category;
+                        } else if(operation == "Set Category") {          
+
+                            foreach (var eachItem in dataManager.TaskList)
+                                {
+                                    if (eachItem.TaskId == editTask.TaskId)
+                                    {   
+                                        AnsiConsole.WriteLine($"Current Category:{eachItem.Category}");
                                         
+                                        string category = AnsiConsole.Prompt(
+                                            new SelectionPrompt<string>()
+                                                .Title("Please select your category")
+                                                .AddChoices("Work", "Errand", "Personal")
+                                        );
+                                        AnsiConsole.WriteLine($"Selected Category:{category}");
+                                        eachItem.Category = category;
                                     }
                                 }
-                            dataManager.SaveAllData();
+
+
+                      
+                          dataManager.SaveAllData();
+                          AnsiConsole.MarkupLine("[green]Category set successfully[/]");                  
+                             
+                        //      string category = AnsiConsole.Prompt(
+                        // new SelectionPrompt<string>()
+                        // .Title("Please select your catergory")
+                        // .AddChoices( "Work", "Personal", "Errand"));
+                        //      foreach(var eachItem in dataManager.TaskList ){
+                        //             if(eachItem.TaskId == editTask.TaskId){
+                        //                 eachItem.Category = category;
+                                        
+                        //             }
+                        //         }
+                        //     dataManager.SaveAllData();
+                        //      AnsiConsole.MarkupLine("[green]Category set successfully[/]");
                             
                         }
                     }
-                    else
+                    else 
                     {
                         Console.WriteLine("Task not found");
                     }
-                     subMenuExit = AnsiConsole.Confirm("Do you want to continue?");
+                     subMenuExit = AnsiConsole.Confirm("Do you want to continue managing task?");
                
                     }while(subMenuExit);
                     
@@ -184,4 +299,17 @@ public class ConsoleUI{
 
         return value;
 }
+    
+    DateTime? AskOptionalDate(string message, DateTime? existing)
+{
+    string input = AnsiConsole.Prompt(
+        new TextPrompt<string>(message)
+            .AllowEmpty()
+    );
+
+    return DateTime.TryParse(input, out var result)
+        ? result
+        : existing;
 }
+
+    }
--- TaskManagementApplication/Domain.cs
+++ TaskManagementApplication/Domain.cs
@@ -3,23 +3,22 @@ namespace TaskManagementApplication;
 public class Task{
 
    public int TaskId;
-   public string TaskName;
-   public string Description;
-   public DateTime Deadline;
-   public string Priority;
-   public string Category;
+   public string TaskName { get; set; }
+   public string Description { get; set; }
+   public DateTime Deadline{ get; set; }
+   public string Priority { get; set; }
+   public string Category { get; set; }
+//    public DateTime StartTime { get; set; }
+//    public DateTime EndTime { get; set; }
+     // Optional fields (safe)
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+   public string Status;
    
 
-//    public Task( string taskName, string description, DateTime deadline, string priority = ""){
-                    
-//             //this.TaskId = taskID;        
-//             this.TaskName = taskName;
-//             this.Description = description;
-//             this.Deadline = deadline;
-//             this.Priority = priority;
-//     }  
 
-    public Task(int taskID, string taskName, string description, DateTime deadline, string priority = "", string category = ""){
+    public Task(int taskID, string taskName, string description, DateTime deadline, string priority, string category,
+    DateTime? starttime, DateTime? endtime, string status){
                     
             this.TaskId = taskID;        
             this.TaskName = taskName;
@@ -27,13 +26,14 @@ public class Task{
             this.Deadline = deadline;
             this.Priority = priority;
             this.Category = category;
+            this.Status = status;
 
     }  
 
      public override string ToString()
     {
-        return $"{TaskName} - {Description} - {Priority} - {Category}";
+        return $"{TaskName} - {Description} - {Priority} - {Category} - {Status}";
     }
 
    
-}
+};
